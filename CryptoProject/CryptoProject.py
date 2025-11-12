3879b99fd4a07a6ea28df2a85e3ddf827abff885
Yu Wu <wuyuapp@bu.edu>
2025-11-12 13:34:23 -0500

    RSA cipher added

--- CryptoProject/CryptoProject.py
+++ CryptoProject/CryptoProject.py
@@ -8,16 +8,15 @@ from utilities import *
 
 def main():
     parser = argparse.ArgumentParser(description="Project for Diffie-Hellman Key Exchange Protocaol and RSA")
-    # positional arguments (required)
+    # Optional argument
     parser.add_argument("--CipherType", choices=["DH","RSA"], help="You can type in DH for Diffie-Hellman/El gamal, or RSA")
-    parser.add_argument("--utilities", choices=["GenPrime", "Modinv", "StrtoInt", "InttoStr", "GenPubKey"], help="You can use utilities to help you to build your cipher.")
+    parser.add_argument("--utilities", choices=["GenPrime", "Modinv", "StrtoInt", "InttoStr", "GenKey"], help="You can use utilities to help you to build your cipher.")
     parser.add_argument("--bits", type=int, help="Enter the bits of your prime number")
     parser.add_argument("--num", type=int, help="Enter your number")
     parser.add_argument("--generator", type=int, help="Enter your generator")
     parser.add_argument("--mode", choices=["d","e"], default="d", help="You can type in d for decrpytion mode or encryption mode")
     parser.add_argument("--plaintext", "-p", type=str, help="Enter the string that you want to encrypt")
     parser.add_argument("--ciphertext", "-c", type=int, help="Enter the string that you want to decrypt")
-    # Alice will publish (b, b^r% P). Alice will comput "(x*((b^lr) mod p)) mod p" and send it to bob. l is the public key, r is the private key. b is the generator.
     parser.add_argument("--privatekey", type=int, help="Enter your private key")
     parser.add_argument("--publickey", type=int, help="Enter your public key")
     parser.add_argument("--Prime", type=int, help="Enter your Prime number")
@@ -37,12 +36,13 @@ def main():
     num=args.num
     generator=args.generator
 
-    if CipherType != None and util == None:
+    if util == None:
         if CipherType == "DH":
             return El_gamal(mode,plaintext,ciphertext,publickey,privatekey,Prime)
         elif CipherType == "RSA":
+            return RSA(mode,plaintext,ciphertext,publickey,privatekey,Prime)
             return
-    elif CipherType is None and util != None:
+    elif util != None:
         if util == "GenPrime":
             if bits is None:
                 parser.error("--bits is required when using GenPrime")
@@ -63,10 +63,10 @@ def main():
                 parser.error("--num is required when using InttoStr")
             else:
                 return print(f"Your converted plaintext for integer is: {int_to_str(num)}")
-        elif util == "GenPubKey":
-            if Prime==None or generator==None or privatekey==None:
-                parser.error("--Prime and --generator are required when using GenPubKey")
+        elif util == "GenKey":
+            if bits == None or privatekey == None or CipherType == None:
+                parser.error("--bits, --privatekey and -- CipherType are required when using GenPubKey")
             else:
-                return print(f"Your public key is: {GenPubKey(generator,privatekey,Prime)}")
+                return genKey(CipherType,bits,privatekey,Prime,generator)
 if __name__ == "__main__":
     main()
--- CryptoProject/utilities.py
+++ CryptoProject/utilities.py
@@ -1,6 +1,7 @@
 
 from FastExpo import *
 from primes import *
+from euclid_functions import *
 
 def GenPrime(bits: int):
     p = generate_prime(bits)
@@ -25,5 +26,47 @@ def int_to_str(number: int):
     plaintext=plaintext_bytes.decode('utf-8')
     return plaintext
 
-def GenPubKey(generator:int,privatekey:int,Prime:int):
+def GenPubKey(generator: int, privatekey: int, Prime: int):
     return fast_exp(generator,privatekey,Prime)
+
+def genKey_RSA(bits: int, privatekey: int):
+
+    prime1,_=generate_safe_prime(bits)
+    prime2,_=generate_safe_prime(bits)
+    Prime = prime1 * prime2
+    phi = (prime1-1)*(prime2-1)
+    publickey=modinv(privatekey,phi)
+    
+    print(f"Your prime is {Prime}")
+    print(f"Your publickey is {publickey}")
+    print(f"Your privatekey is {privatekey}. KEEP THIS TO YOURSELF")
+    print(f"Send (Prime: {Prime}, Public key:{publickey}) to Bob")
+
+    return Prime, privatekey, publickey
+
+def genKey_DH(bits: int, privatekey: int, Prime: int, generator: int):
+    if Prime == None and generator == None:
+        p_safe, q = generate_safe_prime(bits)
+        g = find_generator_safe_prime(p_safe, q)
+        publickey = fast_exp(g,privatekey,p_safe)
+    elif Prime != None and generator != None:
+        p_safe = Prime
+        g = generator
+        publickey = fast_exp(g,privatekey,p_safe)
+
+    print(f"Your prime is {p_safe}")
+    print(f"Your publickey is {publickey}")
+    print(f"Your privatekey is {privatekey}. KEEP THIS TO YOURSELF")
+    print(f"Send (Prime:{p_safe}, Public Key:{publickey}, generator:{g}) to Bob")
+
+
+    return p_safe, privatekey, publickey
+
+def genKey(cipherType: str, bits: int, privatekey: int, Prime: int, generator: int):
+    """
+    random RSA keys
+    """
+    if cipherType == "RSA":
+        return genKey_RSA(bits,privatekey)
+    elif cipherType == "DH":
+        return genKey_DH(bits,privatekey,Prime,generator)
